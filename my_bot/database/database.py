dd054511b6aafcd3aff96c2b18a2c45f8c432c52
vladik <vladusha1223@gmail.com>
2022-03-14 18:48:27 +0200

    .

--- my_bot/database/database.py
+++ my_bot/database/database.py
@@ -1,5 +1,4 @@
-import inspect
-
+from datetime import datetime
 from peewee import *
 
 db = SqliteDatabase('BDB')  # 'Bot Data Base'
@@ -17,6 +16,7 @@ class User(BaseModel):
 
 class Hotel(BaseModel):
 	requester = ForeignKeyField(User, related_name = 'hotels')
+	time = DateTimeField()
 	name = CharField()
 	address = CharField()
 	dist = CharField()
@@ -36,19 +36,29 @@ def user_inf(u_id):
 
 def db_update(user_id, massive):
 	with db:
-		user = User.select().where(User.id == user_id).get()
-		hotel = Hotel.create(requester = user, name = massive[0], address = massive[1], dist = massive[2], price = massive[3])
-		for i_ph in massive[4]:
-			Photo.create(hotel_ph = hotel, photo = i_ph)
+		try:
+			user = User.select().where(User.id == user_id).get()
+			hotel = Hotel.create(requester = user, time = datetime.now(), name = massive[0], address = massive[1], dist = massive[2], price = massive[3])
+			for i_ph in massive[4]:
+				Photo.create(hotel_ph = hotel, photo = i_ph)
+		except DoesNotExist:
+			print("No User")
+
+
+def create_user(name, u_id):
+	with db:
+		User.create(name = name, id = u_id)
 
 
 if __name__ == '__main__':
-	with User.create_table():
-		pass
-	with Hotel.create_table():
-		pass
-	with Photo.create_table():
-		pass
+	pass
+	# with db:
+	# 	User.create_table()
+	# 	Hotel.create_table()
+	# 	Photo.create_table()
+	# create_user('isky', 1234)
+	#
+	# db_update(1234, ('INgyl', 'dsgesg2', '1.2 km', '1200 RUB', 'gfgr'))
 
 	# u1 = User.create(name = 'Isky', id = 1234)
 	#
@@ -60,10 +70,8 @@ if __name__ == '__main__':
 	# db_update(1234, ('Отель «Ингул»', 'ул. Адмиральская, 34', '3,8 км', '1,570 RUB',
 	#             ['https://exp.cdn-hotels.com/hotels/28000000/27320000/27317800/27317796/481e0d06_z.jpg',
 	#              'https://exp.cdn-hotels.com/hotels/27000000/26730000/26724300/26724285/45c48d5c_z.jpg']))
-
 	# info = user_inf(1234)
-	# print(info[0])
 	# for i_h in info[1]:
-	# 	print(i_h.name, i_h.address, i_h.price, i_h.dist)
+	# 	print(i_h.name, i_h.time, i_h.address, i_h.price, i_h.dist)
 	# 	for i_ph in i_h.photos:
 	# 		print(i_ph.photo)
